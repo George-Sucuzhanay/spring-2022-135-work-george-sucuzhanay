23b577eab79e713eb287d37a4ff60b73f90cf07d
George <georgesucuzhanay100@gmail.com>
2022-02-03 16:06:39 -0500

    created starter files for hw exercise 2.10

---
+++ lecture/exercises/hw-e210.cpp
@@ -0,0 +1,17 @@
+/*
+Author: George Sucuzhanay
+Course: CSCI-135
+Instructor: Professor Tong Yi
+Assignment: E2.10
+
+Write a program that prints three items, such as the 
+names of your three bestfriends 
+or favorite movies, on three separate lines.
+
+*/
+#include <iostream>
+
+int main()
+{
+    return 0;
+}

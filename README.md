138e0201f32bcc09a5d31b65330cfa6e9f88bed8
George <georgesucuzhanay100@gmail.com>
2022-02-03 08:53:23 -0500

    started main and lib in multifile folder

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# spring-2022-135-work-george-sucuzhanay
+## spring-2022-135-work-george-sucuzhanay
+# CSCI 135 Main Labs and Hunter CS Daedalus Recitation Work
---
+++ daedalus/multifiles/main.cpp
@@ -0,0 +1,18 @@
+/*
+CSCI 135 Daedalus
+Date: 02/03/2022
+
+Goal: Learning to using multiple files to our main.cpp file
+*/
+
+#include <iostream>
+
+int main()
+{  
+  
+   int c;
+   c = add2(5,10);
+   std::cout << c << "\n";
+   // Learning to use multiple files
+   return 0;
+}

5cdf4977b76c52201368211b04bfe0fd846836d6
George <georgesucuzhanay100@gmail.com>
2022-02-03 08:59:42 -0500

    created multifile to be compilied

--- daedalus/multifiles/main.cpp
+++ daedalus/multifiles/main.cpp
@@ -7,6 +7,13 @@ Goal: Learning to using multiple files to our main.cpp file
 
 #include <iostream>
 
+// this is a "signature" for function add
+// can write as: int add2(int, int);
+// but it's usually a cut and paste from the func name
+// with the ; at the end
+
+int add2(int a, int b);
+
 int main()
 {  
   

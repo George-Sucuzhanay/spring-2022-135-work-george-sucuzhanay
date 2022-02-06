9a29e9ea22c1fd6924f5a207e904d868616ac6d6
George <georgesucuzhanay100@gmail.com>
2022-02-06 00:38:53 -0500

    created starter files for project-1a

---
+++ lecture/projects/project-1a/sum.cpp
@@ -0,0 +1,14 @@
+/*
+Author: George Sucuzhanay
+Course: CSCI-135
+Instructor: Professor Tong Yi
+Assignment: Project 1A - Sum.cpp
+
+Goal: 
+
+*/
+#include <iostream>
+int main()
+{
+    return 0;
+}

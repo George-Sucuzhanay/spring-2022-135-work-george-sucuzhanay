9e76762758d98de2af21935c5c16c9f27d8957bf
George <georgesucuzhanay100@gmail.com>
2022-02-06 00:13:48 -0500

    completed program for cost per 100 miles and max distance of car with gas

--- lecture/exercises/hw-e210.cpp
+++ lecture/exercises/hw-e210.cpp
@@ -4,14 +4,32 @@ Course: CSCI-135
 Instructor: Professor Tong Yi
 Assignment: E2.10
 
-Write a program that prints three items, such as the 
-names of your three bestfriends 
-or favorite movies, on three separate lines.
+Write a program that asks the user to input
+- The number of gallons of gas in the tank
+- The fuel efficiency in miles per gallon
+- The price of gas per gallon 
+
+Print the cost per 100 miles and how far the car 
+can go with the gas in the tank.
 
 */
 #include <iostream>
 
 int main()
 {
+    double gallon,fuelEfficient, price;
+
+    std::cout << "Enter number of gallons of gas in the tank: ";
+    std::cin >> gallon;
+
+    std::cout << "Enter fuel efficiency in miles per gallon: ";
+    std::cin >> fuelEfficient;
+
+    std::cout << "Enter price of gas per gallon: ";
+    std::cin >> price;
+
+    std::cout << price * 100 / fuelEfficient << std::endl;
+    std::cout << gallon * fuelEfficient << std::endl;
+
     return 0;
 }

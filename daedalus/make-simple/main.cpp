d2060828098a1193749d02bd73e56fe5262d3241
George <georgesucuzhanay100@gmail.com>
2022-02-07 09:24:55 -0500

    multi practice

---
+++ daedalus/make-simple/main.cpp
@@ -0,0 +1,26 @@
+/*
+
+Date: 02-07-2022
+Topic: Using make files in cpp
+Seat: C1001 LAB 1 201
+
+Installing make on Linux:
+sudo apt-get install make
+
+Target: dependencies
+        command
+        command
+        command
+
+touch main.cpp => creates a new timestamp for a executable file
+
+main.cpp => main.o => main
+*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World\n";
+    return 0;
+}
--- daedalus/make/main.cpp
+++ daedalus/make/main.cpp
@@ -1,20 +1,3 @@
-/*
-
-Date: 02-07-2022
-Topic: Using make files in cpp
-Seat: C1001 LAB 1 201
-
-Installing make on Linux:
-sudo apt-get install make
-
-Target: dependencies
-        command
-        command
-        command
-
-touch main.cpp => creates a new timestamp for a executable file
-*/
-
 #include <iostream>
 
 int main()
